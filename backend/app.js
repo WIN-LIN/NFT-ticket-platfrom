425042241c9f2700e178f89893051c2fb638d654
Whitney Chou <puppy95176@gmail.com>
2022-12-18 21:36:42 +0800

    fix: rm cors package to post successfully

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,32 @@
 require('dotenv').config();
-const { PORT, API_VERSION } = process.env;
-
+const { PORT, API_VERSION, HOSTNAME } = process.env;
 const express = require('express');
-const cors = require('cors');
 const app = express();
 
-app.use(express.static('public'));
-app.use(express.json());
+app.use(express.static('../frontend/public'));
+app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true }));
-app.use(cors);
+
 
 // API routes
-app.use(`/api/${API_VERSION}`, [
+app.use('/api/'+API_VERSION, [
     require('./server/routes/adminRoute'),
 ]);
 
+
+app.get('/',(req, res) => {
+    res.send("Website is under construction!");
+});
 // Page not found
 app.use((req, res) => {
     res.status(404).json({ message: 'Page not found' });
 });
-
 // Error handler
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: 'Server Error' });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, HOSTNAME, () => {
     console.log(`Server running on port ${PORT}`);
 });
