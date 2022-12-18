79fc6a27e60c246daa1b087f40ec6d84c3e65b11
Whitney Chou <puppy95176@gmail.com>
2022-12-18 11:25:28 +0800

    fix: reject tx error and split up two deployment

--- frontend/pages/host-event.js
+++ frontend/pages/host-event.js
@@ -24,6 +24,10 @@ export default function HostEvent() {
     const { register, handleSubmit, formState: { errors } } = useForm({criteriaMode: "all"});
 
     const { data: signer, isError, isLoading } = useSigner();
+    const [ticketButton, setTicketButton] = useState(false);
+    const [marketButton, setMarketButton] = useState(true);
+    const [ticketAddress, setTicketAddress] = useState('');
+    const [marketAddress, setMarketAddress] = useState('');
 
 
 
@@ -64,56 +68,91 @@ export default function HostEvent() {
         }
     }
 
-    async function deploy(data) {
+    async function deployTicket(data) {
         console.log("data",data);
 
-        if (isConnected) {
-            console.log("Deploying...");
+        try {
+            if (isConnected) {
+                console.log("Deploying...");
 
-            if (!signer) {
-                return alert("Signer not found");
-            }
+                if (!signer) {
+                    return alert("Signer not found");
+                }
 
-            // Check the required fields
-            if (data.eventName.length == 0 || data.eventSymbol.length == 0 || data.ticketSupply.length == 0 ||
-                data.ticketPrice.length == 0 || data.startTime.length == 0 || data.royaltyRate.length == 0 ||
-                data.refundTime.length == 0 || data.refundRate.length == 0) {
-                return alert("Please fill in all the fields");
-            }
+                // Check the required fields
+                if (data.eventName.length == 0 || data.eventSymbol.length == 0 || data.ticketSupply.length == 0 ||
+                    data.ticketPrice.length == 0 || data.startTime.length == 0 || data.royaltyRate.length == 0 ||
+                    data.refundTime.length == 0 || data.refundRate.length == 0) {
+                    return alert("Please fill in all the fields");
+                }
 
-            // Convert local time to Unix time
-            const startTime = new Date(data.startTime).getTime();
-            const refundTime = new Date(data.refundTime).getTime();
-            let endTime;
-            if (data.endTime.length != 0) {
-                endTime = new Date(data.endTime).getTime();
+                // Convert local time to Unix time
+                const startTime = new Date(data.startTime).getTime();
+                const refundTime = new Date(data.refundTime).getTime();
+                let endTime;
+                if (data.endTime.length != 0) {
+                    endTime = new Date(data.endTime).getTime();
+                } else {
+                    endTime = startTime;
+                }
+
+                // Check time
+                const now = new Date().getTime();
+                if (startTime < now || refundTime < now ) {
+                    return alert("Invalid time");
+                }
+                setTicketButton(true);
+                const Ticket = new ethers.ContractFactory(ticketABI, ticketByteCode, signer);
+                const ticket = await Ticket.deploy(data.eventName, data.eventSymbol, data.ticketSupply);
+                await ticket.deployed();
+
+                setTicketAddress(ticket.address);
+                setMarketButton(false);
             } else {
-                endTime = startTime;
+                return alert("Please connect your wallet");
             }
+        } catch (e){
+            setTicketButton(false);
+            return alert("Transaction rejected");
+        }
 
-            // Check time
-            const now = new Date().getTime();
-            if (startTime < now || refundTime < now ) {
-                return alert("Invalid time");
+    }
+
+    async function deployMarket(data) {
+        console.log("data",data);
+        try {
+            if (isConnected) {
+
+                if (!signer) {
+                    return alert("Signer not found");
+                }
+
+                // Convert local time to Unix time
+                const startTime = new Date(data.startTime).getTime();
+                const refundTime = new Date(data.refundTime).getTime();
+                let endTime;
+                if (data.endTime.length != 0) {
+                    endTime = new Date(data.endTime).getTime();
+                } else {
+                    endTime = startTime;
+                }
+
+                const Market = new ethers.ContractFactory(marketABI, marketByteCode, signer);
+                setMarketButton(true);
+                const market = await Market.deploy(
+                    ticketAddress,
+                    startTime,
+                    data.ticketPrice,
+                    data.royaltyRate,
+                    refundTime,
+                    data.refundRate);
+                await market.deployed();
+                console.log("Market deployed to:", market.address);
+            } else {
+                return alert("Please connect your wallet");
             }
-
-            const Ticket = new ethers.ContractFactory(ticketABI, ticketByteCode, signer);
-            const ticket = await Ticket.deploy(data.eventName, data.eventSymbol, data.ticketSupply);
-            await ticket.deployed();
-            console.log("Ticket deployed to:", ticket.address);
-            const Market = new ethers.ContractFactory(marketABI, marketByteCode, signer);
-            console.log("Deploy market...");
-            const market = await Market.deploy(
-                ticket?.address,
-                startTime,
-                data.ticketPrice,
-                data.royaltyRate,
-                refundTime,
-                data.refundRate);
-            await market.deployed();
-            console.log("Market deployed to:", market.address);
-        } else {
-            return alert("Please connect your wallet");
+        } catch (e) {
+            setMarketButton(false);
         }
     }
 
@@ -209,9 +248,13 @@ export default function HostEvent() {
                             {errors.refundRate?.type === 'max' && <p className={styles.alert} role="alert">Refund Rate should not be greater than 100.</p>}
 
                             <div className={styles.confirm}>
-                                <Button  id="event-submit" variant="contained" type="submit" onClick={handleSubmit(deploy)}>Submit</Button>
+                                <Button  id="event-submit" variant="contained" type="submit" disabled={ticketButton} onClick={handleSubmit(deployTicket)}>Deploy Ticket Contract</Button>
                             </div>
-
+                            {ticketAddress? <p className={styles.tip}>Ticket contract deployed successfully. Address: {ticketAddress}</p>: null}
+                            <div className={styles.confirm}>
+                                <Button  id="event-submit" variant="contained" type="submit" disabled={marketButton} onClick={handleSubmit(deployMarket)}>Deploy Market Contract</Button>
+                            </div>
+                            {marketAddress? <p className={styles.tip}>market contract deployed successfully. Address: {marketAddress}</p>: null}
                         </form>
 
                     </Container>:
