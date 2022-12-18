bfd189bfed7913500390112582701be32f2291dc
Whitney Chou <puppy95176@gmail.com>
2022-12-18 10:41:14 +0800

    feature: deploy contracts

--- frontend/pages/host-event.js
+++ frontend/pages/host-event.js
@@ -7,6 +7,7 @@ import { Button, Container, Input, FormLabel } from "@mui/material";
 import { useForm } from "react-hook-form";
 import { ethers, BigNumber } from "ethers";
 import { ticketByteCode,ticketDeployedBytecode, ticketABI, marketByteCode, marketDeployByteCode, marketABI } from "../constants";
+
 export default function HostEvent() {
     const { address, isConnected } = useAccount();
     const { connect } = useConnect({
@@ -20,9 +21,11 @@ export default function HostEvent() {
     const [text, setText] = useState('');
     const [image, setImage] = useState('');
     const [preview, setPreview] = useState('');
-    const { register, handleSubmit, control } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm({criteriaMode: "all"});
 
-    const { data: signer, isError, isLoading } = useSigner()
+    const { data: signer, isError, isLoading } = useSigner();
+
+
 
     useEffect(() => {
         isConnected ? makeConnect(true) : makeConnect(false);
@@ -63,6 +66,7 @@ export default function HostEvent() {
 
     async function deploy(data) {
         console.log("data",data);
+
         if (isConnected) {
             console.log("Deploying...");
 
@@ -70,18 +74,44 @@ export default function HostEvent() {
                 return alert("Signer not found");
             }
 
-            if (data.eventName.length == 0 || data.eventSymbol.length == 0 || data.ticketSupply.length == 0) {
+            // Check the required fields
+            if (data.eventName.length == 0 || data.eventSymbol.length == 0 || data.ticketSupply.length == 0 ||
+                data.ticketPrice.length == 0 || data.startTime.length == 0 || data.royaltyRate.length == 0 ||
+                data.refundTime.length == 0 || data.refundRate.length == 0) {
                 return alert("Please fill in all the fields");
+            }
 
+            // Convert local time to Unix time
+            const startTime = new Date(data.startTime).getTime();
+            const refundTime = new Date(data.refundTime).getTime();
+            let endTime;
+            if (data.endTime.length != 0) {
+                endTime = new Date(data.endTime).getTime();
+            } else {
+                endTime = startTime;
+            }
+
+            // Check time
+            const now = new Date().getTime();
+            if (startTime < now || refundTime < now ) {
+                return alert("Invalid time");
             }
 
             const Ticket = new ethers.ContractFactory(ticketABI, ticketByteCode, signer);
             const ticket = await Ticket.deploy(data.eventName, data.eventSymbol, data.ticketSupply);
             await ticket.deployed();
             console.log("Ticket deployed to:", ticket.address);
-            // 0x3CEd488F105199d0b9bb887a21064072ea9c8ac1
-
-
+            const Market = new ethers.ContractFactory(marketABI, marketByteCode, signer);
+            console.log("Deploy market...");
+            const market = await Market.deploy(
+                ticket?.address,
+                startTime,
+                data.ticketPrice,
+                data.royaltyRate,
+                refundTime,
+                data.refundRate);
+            await market.deployed();
+            console.log("Market deployed to:", market.address);
         } else {
             return alert("Please connect your wallet");
         }
@@ -122,13 +152,19 @@ export default function HostEvent() {
 
                             <h2>Basic Information</h2>
                             <FormLabel className={styles.inputLabel} htmlFor="eventName">Event Name</FormLabel>
-                            <Input id="eventName" name="eventName" className={styles.input} type="text" {...register("eventName")} />
+                            <Input id="eventName" name="eventName" className={styles.input} type="text" {...register("eventName", {required: true, maxLength:50})} />
+                            {errors.eventName?.type === 'required' && <p className={styles.alert} role="alert">Event name is required</p>}
+
                             <FormLabel className={styles.inputLabel} htmlFor="startTime">Start Time</FormLabel>
-                            <Input id="startTime" name="startTime" className={styles.input} type="datetime-local" {...register("startTime")} />
+                            <Input id="startTime" name="startTime" className={styles.input} type="datetime-local" {...register("startTime", {required: true})} />
+                            {errors.startTime?.type === 'required' && <p className={styles.alert} role="alert">Start time is required</p>}
+
                             <FormLabel className={styles.inputLabel} htmlFor="endTime">End Time</FormLabel>
                             <Input id="endTime" name="endTime" className={styles.input} type="datetime-local" {...register("endTime")} />
+
                             <FormLabel className={styles.inputLabel} htmlFor="location">Location</FormLabel>
                             <Input id="location" name="location" className={styles.input} type="text" {...register("location")} />
+
                             <FormLabel className={styles.inputLabel} htmlFor="description">Description</FormLabel>
                             <CKEditor
                                 id="description"
@@ -142,17 +178,36 @@ export default function HostEvent() {
                             />
                             <h2>Contract Setting</h2>
                             <FormLabel className={styles.inputLabel} htmlFor="eventSymbol">Event Symbol</FormLabel>
-                            <Input id="eventSymbol" name="eventSymbol" className={styles.input} type="text" {...register("eventSymbol")} />
+                            <Input id="eventSymbol" name="eventSymbol" className={styles.input} type="text" {...register("eventSymbol",{required: true, maxLength: 10})} />
+                            {errors.eventSymbol?.type === 'required' && <p className={styles.alert} role="alert">Symbol is required.</p>}
+                            {errors.eventSymbol?.type === 'maxLength' && <p className={styles.alert} role="alert">Max length is 10.</p>}
+
                             <FormLabel className={styles.inputLabel} htmlFor="ticketPrice">Ticket Base Price (in ETH)</FormLabel>
-                            <Input id="ticketPrice" name="ticketPrice" className={styles.input} type="number" inputProps={{min:0}} {...register("ticketPrice")} />
+                            <Input id="ticketPrice" name="ticketPrice" className={styles.input} type="number" inputProps={{min:0}} {...register("ticketPrice",{required:true, min:0})} />
+                            {errors.ticketPrice?.type === 'required' && <p className={styles.alert} role="alert">Ticket price is required.</p>}
+                            {errors.ticketPrice?.type === 'min' && <p className={styles.alert} role="alert">Ticket price should not be negative.</p>}
+
                             <FormLabel className={styles.inputLabel} htmlFor="ticketSupply">Total Ticket Supply</FormLabel>
-                            <Input id="ticketSupply" name="ticketSupply" className={styles.input} type="number" inputProps={{min:1}} {...register("ticketSupply")} />
+                            <Input id="ticketSupply" name="ticketSupply" className={styles.input} type="number" inputProps={{min:1}} {...register("ticketSupply", {required:true, min:1})} />
+                            {errors.ticketSupply?.type === 'required' && <p className={styles.alert} role="alert">Ticket supply amount is required.</p>}
+                            {errors.ticketSupply?.type === 'min' && <p className={styles.alert} role="alert">Ticket supply amount at least should be 1.</p>}
+
                             <FormLabel className={styles.inputLabel} htmlFor="royaltyRate">Royalty Rate (%)</FormLabel>
-                            <Input id="royaltyRate" name="royaltyRate" className={styles.input} type="number" inputProps={{min:0, max:100}} {...register("royaltyRate")} />
+                            <Input id="royaltyRate" name="royaltyRate" className={styles.input} type="number" inputProps={{min:0, max:100}} {...register("royaltyRate",{required:true, min:0, max:100})} />
+                            {errors.royaltyRate?.type === 'required' && <p className={styles.alert} role="alert">Royalty Rate is required.</p>}
+                            {errors.royaltyRate?.type === 'min' && <p className={styles.alert} role="alert">Royalty Rate should not be negative.</p>}
+                            {errors.royaltyRate?.type === 'max' && <p className={styles.alert} role="alert">Royalty Rate should not be greater than 100.</p>}
+
                             <FormLabel className={styles.inputLabel} htmlFor="refundTime">Refund Time</FormLabel>
-                            <Input id="refundTime" name="refundTime" className={styles.input} type="datetime-local" {...register("refundTime")} />
+                            <Input id="refundTime" name="refundTime" className={styles.input} type="datetime-local" {...register("refundTime", {required:true})} />
+                            {errors.refundTime?.type === 'required' && <p className={styles.alert} role="alert">Refund Time is required.</p>}
+
                             <FormLabel className={styles.inputLabel} htmlFor="refundRate">Refund Rate (%)</FormLabel>
                             <Input id="refundRate" name="refundRate" className={styles.input} type="number" inputProps={{min:0, max:100}} {...register("refundRate")} />
+                            {errors.refundRate?.type === 'required' && <p className={styles.alert} role="alert">Refund Rate is required.</p>}
+                            {errors.refundRate?.type === 'min' && <p className={styles.alert} role="alert">Refund Rate should not be negative.</p>}
+                            {errors.refundRate?.type === 'max' && <p className={styles.alert} role="alert">Refund Rate should not be greater than 100.</p>}
+
                             <div className={styles.confirm}>
                                 <Button  id="event-submit" variant="contained" type="submit" onClick={handleSubmit(deploy)}>Submit</Button>
                             </div>
