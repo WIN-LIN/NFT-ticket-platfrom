7a21a397616b6e40c24065bce34288364b9ee7f3
Whitney Chou <puppy95176@gmail.com>
2022-12-21 02:01:06 +0800

    feature: add ticket management and ContractMoadle components

---
+++ frontend/components/ContractModel.js
@@ -0,0 +1,205 @@
+import Modal from '@mui/material/Modal';
+import {useEffect, useState} from "react";
+import {Button, Typography, Box, cardHeaderClasses} from "@mui/material";
+import { ticketABI, marketByteCode, marketABI } from "../constants";
+import { ethers } from "ethers";
+import { useSigner } from "wagmi";
+import { LoadingButton } from '@mui/lab';
+
+export default function ContractModal(prop) { //ticketAddress, MarketAddress, eventName
+    const [open, setOpen] = useState(false);
+    const [ ticketIssued, setTicketIssued ] = useState('');
+    const { data: signer, isError, isLoading } = useSigner();
+    const ticketContract = new ethers.Contract(prop.props.ticket_address, ticketABI, signer);
+    const marketContract = new ethers.Contract(prop.props.market_address, marketABI, signer);
+    const [ numOfTicket, setNumOfTicket ] = useState(0);
+    const [ price, setPrice ] = useState(0);
+    const [ fromID, setFromID ] = useState(0);
+    const [ toID, setToID ] = useState(0);
+    const [approveMarket, setApproveMarket] = useState(false);
+    const [mintLoading, setMintLoading] = useState(false);
+    const [approveLoading, setApproveLoading] = useState(false);
+    const [sellLoading, setSellLoading] = useState(false);
+    const handleOpen = async () => {
+        setOpen(true);
+        const number = await ticketContract.ticketCounts();
+        const approve = await ticketContract.isApprovedForAll(prop.props.host, prop.props.market_address );
+        console.log('approve', approve);
+        setTicketIssued(number.toString());
+        setApproveMarket(approve);
+    }
+    const handleClose = () => setOpen(false);
+
+    function handleTicketIssue(e) {
+        setNumOfTicket(parseInt(e.target.value) || 0);
+    }
+    function increNumOfTicket() {
+        setNumOfTicket(numOfTicket + 1);
+    }
+    function decreNumOfTicket() {
+        if (numOfTicket > 0) {
+            setNumOfTicket(numOfTicket - 1);
+        }
+    }
+
+    async function setApprove() {
+        setApproveLoading(true);
+        const tx = await ticketContract.setApprovalForAll(prop.props.market_address, true);
+        await tx.wait();
+        setApproveMarket(true);
+        setApproveLoading(false);
+    }
+
+    async function issueTicket() {
+        try{
+            if (numOfTicket > 0) {
+                setMintLoading(true);
+                const tx = await ticketContract.bulkMintTickets(numOfTicket);
+                await tx.wait();
+                let total = parseInt(ticketIssued) + parseInt(numOfTicket);
+                setTicketIssued(total.toString());
+                setNumOfTicket(0);
+                alert('Ticket Issued');
+                setMintLoading(false);
+            }
+            else {
+                alert('Please enter a number greater than 0');
+            }
+        } catch (e) {
+            setMintLoading(false);
+            console.log(e);
+        }
+    }
+
+    function handlePrice(e) {
+        setPrice(parseInt(e.target.value) || 0);
+    }
+    function handleFromID(e) {
+        setFromID(parseInt(e.target.value) || 0);
+    }
+    function handleToID(e) {
+        setToID(parseInt(e.target.value) || 0);
+    }
+
+    async function sellTickets(){
+        try{
+            if (price > 0 && fromID > 0 && toID > 0) {
+                setSellLoading(true);
+                const tx = await marketContract.sellBulkTickets(fromID,toID, price);
+                await tx.wait();
+                setSellLoading(false);
+                alert('Ticket Listed');
+            }
+            else {
+                alert('Please enter a price greater than 0');
+            }
+        } catch (e) {
+            setSellLoading(false);
+            console.log(e);
+        }
+    }
+
+    return (
+        <div style={{
+            marginTop: -10,
+        }}>
+            <Button onClick={handleOpen}>Transaction</Button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={boxStyle}>
+                        <Typography id="modal-modal-title" variant="h6" component="h2" sx={eventNameStyle}>
+                            {prop.props.name}
+                        </Typography>
+                    {
+                        approveMarket ? null:
+                            <div>
+                                <h3>Approve to Market Contract</h3>
+                                <LoadingButton
+                                    sx={approveButtonStyle} onClick={setApprove} loading={approveLoading}>Approve</LoadingButton>
+                            </div>
+                    }
+                        <div>
+                            <h3>Issue Ticket</h3>
+                            <Button sx={numButtonStyle} onClick={decreNumOfTicket}>-</Button>
+                            <input style={inputStyle} value={numOfTicket} onChange={handleTicketIssue}/>
+                            <Button sx={numButtonStyle} onClick={increNumOfTicket}>+</Button>
+                            <LoadingButton sx={txButtonStyle}
+                                           onClick={issueTicket} loading={mintLoading}>Mint</LoadingButton>
+                            {ticketIssued ? <span style={{padding:"10px"}}>(Already issued: {ticketIssued})</span> : null}
+                        </div>
+                        <div>
+                        <h3>Sell Tickets (in Wei)</h3>
+                            <span style={{padding:"5px"}}>From ID  </span>
+                            <input style={inputStyle1} value={fromID} onChange={handleFromID} placeholder={'From ID'}/>
+                            <span style={{padding:"5px"}}>To ID  </span>
+                            <input style={inputStyle1} value={toID} onChange={handleToID} placeholder={'To ID '}/>
+                            <span style={{padding:"5px"}}>sell at  </span>
+                            <input style={inputStyle} value={price} onChange={handlePrice}/>
+                        <LoadingButton sx={txButtonStyle}
+                                       onClick={sellTickets} loading={sellLoading}>Sell</LoadingButton>
+                        </div>
+                </Box>
+            </Modal>
+        </div>
+    );
+}
+
+const boxStyle = {
+    position: 'absolute',
+    top: '30%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 600,
+    bgcolor: 'background.paper',
+    border: '#F0F0F0 solid 1px',
+    borderRadius: 5,
+    boxShadow: 24,
+    p: 4,
+};
+
+const eventNameStyle = {
+    textAlign:'center',
+    borderBottom: '#F0F0F0 solid 1px',
+}
+
+const inputStyle = {
+    background: 'white',
+    color: 'black',
+    textAlign: 'center',
+    border: '#ddd 1px solid',
+    width: '5rem',
+    height: '2rem'
+}
+
+const inputStyle1 = {
+    background: 'white',
+    color: 'black',
+    textAlign: 'center',
+    border: '#ddd 1px solid',
+    width: '5rem',
+    height: '2rem',
+    marginRight: '0.5rem'
+}
+
+
+const numButtonStyle = {
+    height: '2rem',
+    border: '1px solid #ddd',
+}
+
+const approveButtonStyle = {
+    background: '#62BDFF',
+    color: '#FFFFFF',
+    ':over': {color: 'black', background: '#429FFC'}
+}
+
+const txButtonStyle = {
+    background:'#62BDFF',
+    color:'#FFFFFF',
+    marginLeft: '10px',
+    ':hover':{color: 'black',background: '#429FFC'}
+}
---
+++ frontend/pages/profile.js
@@ -0,0 +1,78 @@
+import Header from "../components/Header";
+import ContractModal from "../components/ContractModel";
+import styles from '../styles/Profile.module.css';
+import { useAccount, useConnect } from 'wagmi';
+import { useEffect, useState, useRef} from "react";
+import { Container } from "@mui/material";
+import {InjectedConnector} from "wagmi/connectors/injected";
+import axios from "axios";
+
+export default function Profile() {
+    const BASE_URL ='http://localhost:4000/api/1.0';
+    const [ connectWallet, makeConnect ] = useState(false);
+    const { address, isConnected } = useAccount();
+    const { connect } = useConnect({
+        connector: new InjectedConnector(),
+    });
+    const [event, setEvent] = useState([]);
+
+    useEffect(() => {
+        isConnected ? makeConnect(true) : makeConnect(false);
+    }, [isConnected]);
+
+    useEffect(() => {
+        if (address) {
+            axios.get(`${BASE_URL}/event/admin?account=${address}`).then((res) => {
+                setEvent(res.data);
+            })
+        } else {
+            setEvent([]);
+        }
+        },[address]);
+    console.log('event',event);
+        return (
+            <>
+                <Header/>
+                {
+                    (!connectWallet) ? <button onClick={() => connect()}>Connect Wallet</button> :
+                        (event.length === 0) ? <p>Loading...</p> :
+                        <Container
+                            sx={{
+                                margin: 'auto',
+                                marginTop: 10,
+                                boxShadow: '5px 5px 10px rgba(0, 0, 0, 0.1)',
+                                display: 'flex',
+                                flexDirection: 'column',
+                                borderRadius: 5,
+                                padding: 10,
+                                width: '70%',
+                                ":hover": {
+                                    boxShadow: '5px 5px 10px rgba(0, 0, 0, 0.2)',
+                                },
+                            }}
+                        >
+                            <h2 className={styles.h2}>Events Management</h2>
+                            <ul>
+                               <li className={styles.title}>
+                                   <div className={styles.titleDiv}><span className={styles.titleName}>Name</span></div>
+                                   <div className={styles.titleDiv}><span className={styles.titleName}>Start Time</span></div>
+                                   <div className={styles.titleDiv}><span className={styles.titleName}>End Time</span></div>
+                                   <div className={styles.titleDiv}><span className={styles.titleName}>Ticket Total Supply</span></div>
+                                   <div className={styles.titleDiv}><span className={styles.titleName}>Sold</span></div>
+                                   <div className={styles.titleDiv}><span className={styles.titleName}>Contract</span></div>
+                               </li>
+                                <li className={styles.entries}>
+                                    <div className={styles.entriesDiv}><span className={styles.entriesName}>Event 1</span></div>
+                                    <div className={styles.entriesDiv}><span className={styles.entriesName}>2021-10-10</span></div>
+                                    <div className={styles.entriesDiv}><span className={styles.entriesName}>2021-10-10</span></div>
+                                    <div className={styles.entriesDiv}><span className={styles.entriesName}>100</span></div>
+                                    <div className={styles.entriesDiv}><span className={styles.entriesName}>0</span></div>
+                                    <div className={styles.entriesDiv}><ContractModal props={event['data'][8]} /></div>
+                                </li>
+                            </ul>
+                        </Container>
+                }
+
+            </>
+        );
+}
