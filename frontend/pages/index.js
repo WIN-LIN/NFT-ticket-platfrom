90c4d2b5655c9ecd96564a891be1ecc82b5e844b
Whitney Chou <puppy95176@gmail.com>
2022-12-21 15:17:49 +0800

    feature: add carousel

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,20 @@
 import styles from '../styles/Home.module.css'
-// import HeaderMoralis from '../components/Header(Moralis)'
 import Header from "../components/Header";
+import Carousel from "../components/Carousel";
 export default function Home() {
+    const slides = [
+        {url: '/slide1.png', title: 'Slide 1'},
+        {url: '/slide2.png', title: 'Slide 2'},
+        {url: '/slide3.png', title: 'Slide 3'},
+    ]
   return (
-    <div className={styles.container}>
+    <>
         <Header/>
-    </div>
+        <div className={styles.carouselContainerStyle}>
+            <Carousel slides={slides}/>
+        </div>
+        Index
+    </>
 
   )
 }
