d4ea3afeab78e7b9e25ede3fb0596958bab89e36
Whitney Chou <puppy95176@gmail.com>
2022-12-22 00:36:50 +0800

    add event-detail/[id] page

---
+++ frontend/pages/event-detail/[id].js
@@ -0,0 +1,10 @@
+import Header from "../../components/Header";
+
+export default function Id() {
+    return (
+        <>
+            <Header/>
+            <div>EventDetail</div>
+        </>
+    );
+}
